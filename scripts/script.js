025e0727b9c89a4722ad94b741a31a76dc9bd321
Salvadore <salvadoregunacci5@gmail.com>
2023-03-30 15:17:06 +0300

    сделал header

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,17 @@
 // Variables
 // =========================
 
+const $selectLang = document.querySelector('.select_lang');
 
 // =========================
 // Events
 // =========================
 
+if ($selectLang) {
+  $selectLang.addEventListener("click", () => {
+    $selectLang.toggleAttribute("active");
+  });
+}
 
 // =========================
 // Functions
