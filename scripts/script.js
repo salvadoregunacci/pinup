21677cd69ef1eef77b41661181acefd542ee8c16
Salvadore <salvadoregunacci5@gmail.com>
2023-03-31 16:43:16 +0300

    сделал секцию с играми

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@
 // =========================
 
 const $selectLang = document.querySelector('.select_lang');
+const $rolledOpenButtons = document.querySelectorAll('.rolled_btn');
+const $rolledBackButtons = document.querySelectorAll('.rolled_back_btn');
 
 // =========================
 // Events
@@ -14,6 +16,26 @@ if ($selectLang) {
   });
 }
 
+if ($rolledOpenButtons) {
+  _rolledHandler($rolledOpenButtons);
+}
+
+if ($rolledBackButtons) {
+  _rolledHandler($rolledBackButtons);
+}
+
 // =========================
 // Functions
 // =========================
+
+function _rolledHandler(handlersArr) {
+  handlersArr.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const $curRolled = btn.closest(".rolled");
+
+      if (!$curRolled) return;
+
+      $curRolled.toggleAttribute("active");
+    });
+  });
+}
