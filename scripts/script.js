13008fffea0774121428f6b26dd8c27d7747831f
Salvadore <salvadoregunacci5@gmail.com>
2023-03-31 21:33:31 +0300

    готова секция с инфой и faq

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@
 const $selectLang = document.querySelector('.select_lang');
 const $rolledOpenButtons = document.querySelectorAll('.rolled_btn');
 const $rolledBackButtons = document.querySelectorAll('.rolled_back_btn');
+const $faqItems = document.querySelectorAll('.faq_section__item');
 
 // =========================
 // Events
@@ -24,6 +25,12 @@ if ($rolledBackButtons) {
   _rolledHandler($rolledBackButtons);
 }
 
+if ($faqItems) {
+  $faqItems.forEach(item => {
+    item?.addEventListener("click", (e) => _openFaqBlock(e));
+  });
+}
+
 // =========================
 // Functions
 // =========================
@@ -38,4 +45,16 @@ function _rolledHandler(handlersArr) {
       $curRolled.toggleAttribute("active");
     });
   });
+}
+
+function _openFaqBlock(e) {
+  const $curItem = e.target.closest(".faq_section__item");
+
+  if ($curItem && $curItem.classList.contains("active")) {
+    $curItem.classList.remove("active");
+    return;
+  }
+  
+  $faqItems.forEach(item => item.classList.remove("active"));
+  $curItem.classList.add("active");
 }
